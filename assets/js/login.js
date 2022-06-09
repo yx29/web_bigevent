ab1dbab1a717daa3ddbcb3481f6957f6912c6c5c
yx29 <1547918957@qq.com>
2022-06-09 19:42:03 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,98 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
+$(function () { 
+  $('#link_login').on('click', function () { 
+   $('.login').hide()
+    $('.reg').show()
+  })
+  $('#link_reg').on('click', function () { 
+    $('.reg').hide()
+     $('.login').show()
   })
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
+  // 表单验证
+  // 从layui中获取form对象
+  let form = layui.form
+  let layer = layui.layer
   form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
+  username: function(value, item){ //value：表单的值、item：表单的DOM对象
+    if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
+      return '用户名不能有特殊字符';
     }
+    if(/(^\_)|(\__)|(\_+$)/.test(value)){
+      return '用户名首尾不能出现下划线\'_\'';
+    }
+    if(/^\d+\d+\d$/.test(value)){
+      return '用户名不能全为数字';
+    }
+    
+    //如果不想自动弹出默认提示框，可以直接返回 true，这时你可以通过其他任意方式提示（v2.5.7 新增）
+    if(value === 'xxx'){
+      alert('用户名不能为敏感词');
+      return true;
+    }
+  }
+  
+  //我们既支持上述函数式的方式，也支持下述数组的形式
+  //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+  ,pwd: [
+    /^[\S]{6,12}$/
+    ,'密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function (value) {
+    // value拿到的是用户输入的值
+    // 必须拿到密码框中的值
+      // 两次值进行一次相等的判断
+      // 如果不相等则返回一个值
+      let pwd = $('.reg [name=password]').val()
+      if (pwd != value) {
+        return '两次输入的密码不相等'
+      }
+  }
   })
-
+  
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
+  $('#form-reg').on('submit', function (e) {
+    let data = { username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=password]').val() }
     e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
+    $.post('http://big-event-api-t.itheima.net/api/reguser',data, function (res) {
       if (res.status !== 0) {
-        return layer.msg(res.message)
+        return layer.msg(res.message);
       }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
+      layer.msg('注册成功');
+      $('#link_reg').click()
     })
+
   })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
+  //监听登录表单的提交事件
+  // $('#form-login').on('submit', function (e) {
+  //   e.preventDefault()
+  //   let data = { username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=password]').val() }
+  //   $.post('http://big-event-api-t.itheima.net/api/login', data, function (res) {
+  //     if (res.status !== 0) {
+  //       return layer.msg(res.message);
+  //     }
+  //      layer.msg('登录成功');
+  //   })
+  //  })
+  $('#form-login').submit(function (e) {
     e.preventDefault()
     $.ajax({
-      url: '/api/login',
+      url:'http://big-event-api-t.itheima.net/api/login',
       method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+      data: $(this).serialize(),   //快速获取表单中的数据
+      success: function (res) {
+         if (res.status !== 0) {
+          return layer.msg(res.message);
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
+        layer.msg('登录成功')
+        // 将token字符串存储到localstorage中，要的时候就从中拿就是
+        localStorage.setItem('token',res.token)
+        // 登录成功后就跳转到首页
+        // location.href='/index.html'  
       }
+      
     })
+
   })
+
 })
